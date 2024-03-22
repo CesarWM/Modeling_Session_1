4a04e7161336233a78734f62e6803c8603079930
cesar <cesarwikborg@gmail.com>
2024-03-22 12:23:14 +0100

    2nd

--- Point.py
+++ Point.py
@@ -17,7 +17,7 @@ class Point:
 
     def __repr__(self):
         """
-        This i swhen the point is in a list or other container
+        This is when the point is in a list or other container
         :return:
         """
         return self.__str__()
@@ -45,36 +45,37 @@ class Point:
         """
         return self.distance_origin() == other.distance_origin()
 
+if __name__ == "__main__":
+    a = Point(2, 3)
+    b = Point(7, 9)
+    print(f"a=({a.x}, {a.y})")
+    print(f"b=({b.x}, {b.y})")
 
-a = Point(2, 3)
-b= Point(7, 9)
-print(f"a=({a.x}, {a.y})")
-print(f"b=({b.x}, {b.y})")
+    # creating a list of random 5 points
+    points = [] # initialize an empty list
+    for _ in range(5):
+        # x = random.randint(-100, 100)
+        # y = random.randint(-100, 100)
+        # random_points = Point(x, y)
+        # points.append(random_points)
 
-# creating a list of random 5 points
-points = [] # initialize an empty list
-for _ in range(5):
-    # x = random.randint(-100, 100)
-    # y = random.randint(-100, 100)
-    # random_points = Point(x, y)
-    # points.append(random_points)
+        # or in a single line like this
+        points.append(Point(random.randint(-100, 100),
+                            random.randint(-100, 100)))
 
-    # or in a single line like this
-    points.append(Point(random.randint(-100, 100),
-                        random.randint(-100, 100)))
+    for point in points:
+        print(f"n({point.x}, {point.y})")
 
-for point in points:
-    print(f"n({point.x}, {point.y})")
+    # try to print the first point
+    print("printing a point value:", points[0])
+    print(points)
 
-# try to print the first point
-print("printing a point value:", points[0])
-print(points)
+    a = Point(3, 4)
+    print(f"distance origin a={a.distance_origin()}")
+    b = Point(5, 12)
+    print(f"distance origin b={b.distance_origin()}")
+    print(f"a > b = {a > b}, a < b = {a < b}")
 
-a = Point(3, 4)
-print(f"distance origin a={a.distance_origin()}")
-b = Point(5, 12)
-print(f"distance origin b={b.distance_origin()}")
-print(f"a > b = {a > b}, a < b = {a < b}")
+    points.sort()
+    print(f"largest point in the list is {points[-1]}") # the largest is the last in ordered list
 
-points.sort()
-print(f"largest point in the list is {points[-1]}") # the largest is the last in ordered list
